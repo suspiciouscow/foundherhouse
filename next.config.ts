17472aff40306cd590de8dd91da7c40d0c76cfae
miki <miki.safronov1@gmail.com>
2025-01-08 10:55:07 -0800

    Update next.config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true // This is needed when using export
+  },
+  // keep any other config options you might add later
 };
 
 export default nextConfig;
